8018220cee0af973ec065181565d5d98fe1a35e7
Justin Griffith <pure.justin@gmail.com>
2026-02-06 11:35:52 -0600

    auto-backup: 11:35:52

---
+++ functions/src/index.ts
@@ -0,0 +1,26 @@
+/**
+ * Cloud Functions Entry Point
+ *
+ * Exports all Firebase Cloud Functions for the Power to the People app.
+ */
+
+import * as admin from "firebase-admin";
+
+// Initialize Firebase Admin SDK
+admin.initializeApp();
+
+// Export SMT Connector functions
+export {
+  fetchSmtUsage,
+  smtWebhook,
+} from "./smtConnector";
+
+// Export Lead Management functions
+export {
+  createLead,
+  updateLead,
+  addLeadNote,
+  assignLead,
+  recalculateLeadScores,
+  leadWebhook,
+} from "./leads";
